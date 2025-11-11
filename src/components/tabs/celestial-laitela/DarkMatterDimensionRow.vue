2dda5d2920ebf82edfb44c5205e28e297027a274
Supersonic Seven <b.rockholz@icloud.com>
2025-11-11 10:48:54 -0500

    Update DarkMatterDimensionRow.vue
    Besiding the fact that it’s raised

--- src/components/tabs/celestial-laitela/DarkMatterDimensionRow.vue
+++ src/components/tabs/celestial-laitela/DarkMatterDimensionRow.vue
@@ -142,7 +142,7 @@ export default {
     // however; it looks better in-game if we just format it as Infinity instead, as the resource used for these costs
     // is itself hardcapped at e308 and we specifically want to format here (and nowhere else) as Infinity.
     formatDMCost(cost) {
-      return cost.gt(Number.MAX_VALUE) ? Notations.current.infinite : format(cost, 2);
+      return cost.gt(Laitela.darkMatterCap) ? Notations.current.infinite : format(cost, 2);
     },
     dimensionProduction(tier) {
       if (tier === 8 && DarkMatterDimensions.highestUnlocked !== 8) return new Decimal(0);
