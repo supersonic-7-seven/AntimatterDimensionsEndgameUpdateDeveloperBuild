514d6462348d69fb0ba92f89deff927b421d8024
Supersonic Seven <b.rockholz@icloud.com>
2025-11-17 21:47:30 -0500

    Update dark-matter-dimension.js
    My ahh hands mistyping again

--- src/core/celestials/laitela/dark-matter-dimension.js
+++ src/core/celestials/laitela/dark-matter-dimension.js
@@ -277,7 +277,7 @@ export const DarkMatterDimensions = {
           DarkMatterDimension(tier - 1).amount = DarkMatterDimension(tier - 1).amount.plus(productionDM);
         }
         Currency.darkEnergy.add(ticks.mul(dim.powerDE));
-        if (Currency.darkMatter.max.gte(Laitela.darkMatterCap) {
+        if (Currency.darkMatter.max.gte(Laitela.darkMatterCap)) {
           Currency.darkMatter = Laitela.darkMatterCap;
         }
         dim.timeSinceLastUpdate -= dim.interval.times(ticks).toNumber();
