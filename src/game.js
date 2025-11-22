2265b12f612c4ab9a942364425d6bf0316212b8a
Supersonic Seven <b.rockholz@icloud.com>
2025-11-22 14:50:31 -0500

    Update game.js
    Why did I leave that there

--- src/game.js
+++ src/game.js
@@ -682,26 +682,6 @@ export function gameLoop(passedDiff, options = {}) {
   GalaxyGenerator.loop(realDiff);
   GameEnd.gameLoop(realDiff);
 
-  if (player.celestials.laitela.dimensions.length === 4) {
-    player.celestials.laitela.dimensions = Array.range(0, 8).map(() =>
-      ({
-        amount: new Decimal(0),
-        intervalUpgrades: DC.D0,
-        powerDMUpgrades: DC.D0,
-        powerDEUpgrades: DC.D0,
-        timeSinceLastUpdate: 0,
-        ascensionCount: DC.D0
-      }));
-    if (ImaginaryUpgrade(15).isBought) DarkMatterDimension(1).amount = new Decimal(1);
-    if (ImaginaryUpgrade(16).isBought) DarkMatterDimension(2).amount = new Decimal(1);
-    if (ImaginaryUpgrade(17).isBought) DarkMatterDimension(3).amount = new Decimal(1);
-    if (ImaginaryUpgrade(18).isBought) DarkMatterDimension(4).amount = new Decimal(1);
-    if (ImaginaryUpgrade(26).isBought) DarkMatterDimension(5).amount = new Decimal(1);
-    if (ImaginaryUpgrade(27).isBought) DarkMatterDimension(6).amount = new Decimal(1);
-    if (ImaginaryUpgrade(28).isBought) DarkMatterDimension(7).amount = new Decimal(1);
-    if (ImaginaryUpgrade(29).isBought) DarkMatterDimension(8).amount = new Decimal(1);
-  }
-
   if (!Enslaved.canAmplify) {
     Enslaved.boostReality = false;
   }
