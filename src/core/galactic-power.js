7b79c8ec3cb427644ddf42e04df6854c5afca6e6
Supersonic Seven <b.rockholz@icloud.com>
2025-11-24 15:46:53 -0500

    Update galactic-power.js
    Always remember to add a 1 to your Decimal Logarithms

--- src/core/galactic-power.js
+++ src/core/galactic-power.js
@@ -42,8 +42,8 @@ export const GalacticPower = {
 export function getGalacticPowerGainPerSecond() {
   const allGalaxies = Replicanti.galaxies.total + player.galaxies + player.dilation.totalTachyonGalaxies;
   const galaxyFactor = Math.max(allGalaxies / 100000, 1);
-  const celMatterFactor = Math.max(Math.pow(Decimal.log10(player.endgame.celestialMatter) / 10, 2), 1);
-  const imaginaryFactor = Math.max(Math.pow(Decimal.log10(player.reality.imaginaryMachines), 1.5), 1);
+  const celMatterFactor = Math.max(Math.pow(Decimal.log10(player.endgame.celestialMatter.add(1)) / 10, 2), 1);
+  const imaginaryFactor = Math.max(Math.pow(Decimal.log10(player.reality.imaginaryMachines.add(1)), 1.5), 1);
   const galaxyExponent = Math.min(1 + Math.pow(allGalaxies / 2000000, 2), 100);
   return Pelle.isDoomed ? new Decimal(0) : Decimal.pow(galaxyFactor * celMatterFactor * imaginaryFactor / 1000, galaxyExponent);
 }
