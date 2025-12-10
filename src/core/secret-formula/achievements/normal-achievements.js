ce2a4ab0de6072230c4e6d2a26afeae70345e26a
Supersonic Seven <b.rockholz@icloud.com>
2025-12-10 10:57:20 -0500

    Update normal-achievements.js
    Don’t divide

--- src/core/secret-formula/achievements/normal-achievements.js
+++ src/core/secret-formula/achievements/normal-achievements.js
@@ -499,7 +499,7 @@ export const normalAchievements = [
     checkEvent: GAME_EVENT.GAME_TICK_AFTER,
     reward: "Antimatter Dimensions gain a multiplier based on current antimatter.",
     effect: () => Currency.antimatter.value.pow(0.00002).plus(1).clampMax(Decimal.pow(10, 1e30)).pow(
-      Decimal.pow(2, Math.log10(Decimal.log10(Currency.antimatter.value.pow(0.00002).plus(1)) / 1e30))),
+      Decimal.max(Decimal.pow(2, Math.log10(Decimal.log10(Currency.antimatter.value.pow(0.00002).plus(1)) / 1e30)), 1)),
     formatEffect: value => `${formatX(value, 2, 2)}`
   },
   {
@@ -589,7 +589,7 @@ export const normalAchievements = [
     checkEvent: GAME_EVENT.GAME_TICK_AFTER,
     reward: "Antimatter Dimensions are stronger the more unspent antimatter you have.",
     effect: () => Currency.antimatter.value.pow(0.00002).plus(1).clampMax(Decimal.pow(10, 1e30)).pow(
-      Decimal.pow(2, Math.log10(Decimal.log10(Currency.antimatter.value.pow(0.00002).plus(1)) / 1e30))),
+      Decimal.max(Decimal.pow(2, Math.log10(Decimal.log10(Currency.antimatter.value.pow(0.00002).plus(1)) / 1e30)), 1)),
     formatEffect: value => `${formatX(value, 2, 2)}`
   },
   {
