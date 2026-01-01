c8048e1e5725ec5c9e38c0aacc9756c502f19fae
Supersonic Seven <b.rockholz@icloud.com>
2026-01-01 12:37:39 -0500

    Update EnslavedTab.vue
    Fixing slashes

--- src/components/tabs/celestial-enslaved/EnslavedTab.vue
+++ src/components/tabs/celestial-enslaved/EnslavedTab.vue
@@ -90,7 +90,7 @@ export default {
         "o-enslaved-mechanic-button--clickable": this.canModifyGameTimeStorage,
         "o-enslaved-mechanic-button--storing-time": this.isStoringBlackHole,
         "l-fixed-setting": !this.canModifyGameTimeStorage,
-        "o-pelle-disabled": this.isDoomed
+        "o-pelle-disabled": this.isDoomed && !PelleStrikes.dilation.isDestroyed
       };
     },
     storeRealTimeClass() {
@@ -99,7 +99,7 @@ export default {
         "o-enslaved-mechanic-button--clickable": !this.isDoomed,
         "o-enslaved-mechanic-button--storing-time": this.isStoringReal,
         "l-fixed-setting": !this.canChangeStoreRealTime,
-        "o-pelle-disabled": this.isDoomed
+        "o-pelle-disabled": this.isDoomed && !PelleStrikes.dilation.isDestroyed
       };
     },
     dischargeClass() {
@@ -107,7 +107,7 @@ export default {
         "o-enslaved-mechanic-button": true,
         "o-enslaved-mechanic-button--clickable": !this.isDoomed,
         "l-fixed-setting": !this.canDischarge || this.hasNoCharge,
-        "o-pelle-disabled": this.isDoomed
+        "o-pelle-disabled": this.isDoomed && !PelleStrikes.dilation.isDestroyed
       };
     },
     doomedDisabledClass() {
@@ -116,7 +116,7 @@ export default {
     mechanicButtonClass() {
       return {
         "o-enslaved-mechanic-button": true,
-        "o-enslaved-mechanic-button--clickable": !this.isDoomed
+        "o-enslaved-mechanic-button--clickable": !this.isDoomed && !PelleStrikes.dilation.isDestroyed
       };
     }
   },
