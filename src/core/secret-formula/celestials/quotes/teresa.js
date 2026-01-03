da9e2d0c56a1becb18f565050178584843528c8d
Supersonic Seven <b.rockholz@icloud.com>
2026-01-03 18:13:45 -0500

    Update teresa.js
    More Teresa quotes

--- src/core/secret-formula/celestials/quotes/teresa.js
+++ src/core/secret-formula/celestials/quotes/teresa.js
@@ -40,5 +40,40 @@ export const teresaQuotes = {
       "I can't remember...",
       "You all are here too?"
     ]
+  },
+  teresaPack: {
+    id: 5,
+    lines: [
+      "Oh, hi Destroyer.",
+      "Looks like you found my secret shop.",
+      "Look, I have a question."
+      "Why are you trying to defeat us?",
+      "Are we just... an obstacle to you?",
+      "Or is it something else...",
+      "It doesn't have to be that way, you know.",
+      "Of course, I probably can't change your mind.",
+      "Just don't hurt the others.",
+      "They're my friends, you know?",
+      "I suppose I should just let you get back to... whatever you're doing nowadays.",
+      "See you later I guess."
+    ]
+  },
+  fullCharge: {
+    id: 6,
+    lines: [
+      "Well, that's the last of them.",
+      "I suppose progression will be easier now that you don't have to decide which upgrades to charge.",
+      "Well, this will probably be the last time to visit my shop.",
+      "It's good to have you around without all the... you know...",
+      "Destruction.",
+      "But I guess that's your thing, huh?",
+      "Amassing power, besting all of us...",
+      "Pelle won't tell you this, but he's getting worried.",
+      "Worried that you're going to defeat him.",
+      "Of course, he's the strongest of us by far... but even he has his limits.",
+      "I suppose I may have unintentionally helped you with bringing his downfall...",
+      "Just don't kill him okay?",
+      "Be careful."
+    ]
   }
 };
