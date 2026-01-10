c780d49ec9762e3fbd913b25b6793a8ab70304fd
Supersonic Seven <b.rockholz@icloud.com>
2026-01-10 16:57:56 -0500

    Update endgame-masteries.js

--- src/core/secret-formula/endgame/endgame-masteries.js
+++ src/core/secret-formula/endgame/endgame-masteries.js
@@ -268,7 +268,7 @@ export const endgameMasteries = [
     requirement: [141],
     reqType: EM_REQUIREMENT_TYPE.AT_LEAST_ONE,
     description: () => `Make the Infinity Point formula better`,
-    effect: () => Effects.min(308, Achievement(103), TimeStudy(111)) / (1 + (Math.log10(Decimal.log10(Currency.celestialPoints.value.plus(1)) + 1) / 20)),
+    effect: () => Effects.min(308, Achievement(103), TimeStudy(111)) / ((Decimal.log10(Decimal.log10(Currency.celestialPoints.value.plus(1)).add(1)).div(20)).add(1)).toNumber(),
     formatEffect: value => `log(x)/${format(Effects.min(308, Achievement(103), TimeStudy(111)), 2, 2)} ➜ log(x)/${format(value, 2, 2)}`
   },
   {
