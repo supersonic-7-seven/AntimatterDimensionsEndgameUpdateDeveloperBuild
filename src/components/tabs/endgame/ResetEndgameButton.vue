2e12461990168b6ac87b9cdf8b05b5fe444a9166
Supersonic Seven <b.rockholz@icloud.com>
2026-01-12 12:01:24 -0500

    Update ResetEndgameButton.vue

--- src/components/tabs/endgame/ResetEndgameButton.vue
+++ src/components/tabs/endgame/ResetEndgameButton.vue
@@ -14,7 +14,7 @@ export default {
   },
   methods: {
     update() {
-      this.canEndgame = player.celestials.pelle.records.totalEndgameAntimatter.log10() >= 9e15;
+      this.canEndgame = player.celestials.pelle.records.totalEndgameAntimatter.log10().gte(9e15);
       this.isDoomed = Pelle.isDoomed;
     },
     resetEndgame() {
