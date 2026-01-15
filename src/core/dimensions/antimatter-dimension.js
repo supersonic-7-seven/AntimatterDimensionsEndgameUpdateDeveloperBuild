5499d074e250673f401b4a435115819489ebdc3b
Supersonic Seven <b.rockholz@icloud.com>
2026-01-15 18:12:34 -0500

    Update antimatter-dimension.js
    Value fix

--- src/core/dimensions/antimatter-dimension.js
+++ src/core/dimensions/antimatter-dimension.js
@@ -613,7 +613,7 @@ class AntimatterDimensionState extends DimensionState {
       if (production.gt(10)) {
         const log10 = production.log10();
         const eg = Currency.endgames.value;
-        const endgameMult = Pelle.isDoomed ? 1 + (Math.log10(Math.min(eg, 1e6) * (Math.log2(eg + 1) - 5) + 1) / 80) : 1 + (Math.log10(Math.min(eg, 1e6) * (Math.log2(eg + 1) - 5) + 1) / 200);
+        const endgameMult = Pelle.isDoomed ? 1 + (Math.log10(Math.min(eg, 1e6) * Math.max(Math.log2(eg + 1) - Math.log2(5e5), 1) + 1) / 80) : 1 + (Math.log10(Math.min(eg, 1e6) * Math.max(Math.log2(eg + 1) - Math.log2(5e5), 1) + 1) / 200);
         const endgameMultValue = EndgameMilestone.endgameAntimatter.isReached ? endgameMult : 1;
         production = Decimal.pow10(Decimal.pow(log10, getAdjustedGlyphEffect("effarigantimatter") * Effects.product(EndgameMastery(101), EndgameUpgrade(15), SingularityMilestone.antimatterExponentPower) * endgameMultValue));
       }
