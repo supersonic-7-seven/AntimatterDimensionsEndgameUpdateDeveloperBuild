964fd2c7392a0e2ea1215054be9fff200f96e3a3
Supersonic Seven <b.rockholz@icloud.com>
2026-01-23 13:17:42 -0500

    Update globals.js
    The Alpha has come

--- src/core/globals.js
+++ src/core/globals.js
@@ -38,6 +38,7 @@ export * from "./celestials/pelle/rifts";
 export * from "./celestials/pelle/galaxy-generator";
 export * from "./celestials/pelle/game-end";
 export * from "./celestials/pelle/pelle-destruction-upgrades";
+export * from "./celestials/alpha";
 export * from "./celestials/celestials";
 
 export * from "./automator";
