d555640f097725da1b7c5336a180c22b71ea676b
Supersonic Seven <b.rockholz@icloud.com>
2026-01-29 14:29:31 -0500

    Update currency.js
    Numerical instance

--- src/core/currency.js
+++ src/core/currency.js
@@ -123,7 +123,8 @@ export class Currency {
 
   subtract(amount) {
     if (new Decimal(amount).gte(DC.E9E15)) return;
-    if (new Decimal(amount).gte(this.value)) this.value = Decimal.floor(this.value.div(1e15));
+    if (new Decimal(amount).gte(this.value) && this.value instanceof Decimal) this.value = Decimal.floor(this.value.div(1e15));
+    if (new Decimal(amount).gte(this.value) && typeof this.value === "number") this.value = Math.floor(this.value / 1e15);
     this.value = this.operations.max(this.operations.subtract(this.value, amount), 0);
   }
 
