498fc2f968a1a277b4251133d4323a1331354a1c
Supersonic Seven <b.rockholz@icloud.com>
2026-01-30 18:03:42 -0500

    Update glyph-sacrifices.js
    Zero sac seltzer

--- src/core/secret-formula/reality/glyph-sacrifices.js
+++ src/core/secret-formula/reality/glyph-sacrifices.js
@@ -3,7 +3,7 @@ export const glyphSacrifice = {
     id: "power",
     effect: added => {
       if (Pelle.isDisabled("glyphsac") && !PelleRealityUpgrade.scourToEmpower.isBought) return new Decimal(0);
-      const sac = player.reality.glyphs.sac.power.add(added ?? 0);
+      const sac = player.disablePostReality ? DC.D0 : player.reality.glyphs.sac.power.add(added ?? 0);
       const capped = Decimal.clampMax(sac, GlyphSacrificeHandler.maxSacrificeForEffects);
       const base = new Decimal(Decimal.log10(capped.add(1))).div(100);
       return Decimal.floor(Decimal.pow(base, 1.2).times(750));
@@ -22,7 +22,7 @@ export const glyphSacrifice = {
     id: "infinity",
     effect: added => {
       if (Pelle.isDisabled("glyphsac") && !PelleRealityUpgrade.scourToEmpower.isBought) return new Decimal(1);
-      const sac = player.reality.glyphs.sac.infinity.add(added ?? 0);
+      const sac = player.disablePostReality ? DC.D0 : player.reality.glyphs.sac.infinity.add(added ?? 0);
       const capped = Decimal.clampMax(sac, GlyphSacrificeHandler.maxSacrificeForEffects);
       return new Decimal(Decimal.log10(Decimal.pow(capped, 0.2).div(100).add(1))).add(1);
     },
@@ -33,7 +33,7 @@ export const glyphSacrifice = {
     id: "time",
     effect: added => {
       if (Pelle.isDisabled("glyphsac") && !PelleRealityUpgrade.scourToEmpower.isBought) return new Decimal(1);
-      const sac = player.reality.glyphs.sac.time.add(added ?? 0);
+      const sac = player.disablePostReality ? DC.D0 : player.reality.glyphs.sac.time.add(added ?? 0);
       const capped = Decimal.clampMax(sac, GlyphSacrificeHandler.maxSacrificeForEffects);
       return Decimal.pow(Decimal.pow(capped, 0.2).div(100).add(1), 2);
     },
@@ -44,7 +44,7 @@ export const glyphSacrifice = {
     id: "replication",
     effect: added => {
       if (Pelle.isDisabled("glyphsac") && !PelleRealityUpgrade.scourToEmpower.isBought) return new Decimal(0);
-      const sac = player.reality.glyphs.sac.replication.add(added ?? 0);
+      const sac = player.disablePostReality ? DC.D0 : player.reality.glyphs.sac.replication.add(added ?? 0);
       const capped = Decimal.clampMax(sac, GlyphSacrificeHandler.maxSacrificeForEffects);
       const base = new Decimal(Decimal.log10(capped.add(1))).div(100);
       return Decimal.floor(Decimal.pow(base, 1.2).times(1500));
@@ -63,7 +63,7 @@ export const glyphSacrifice = {
     id: "dilation",
     effect: added => {
       if (Pelle.isDisabled("glyphsac") && !PelleRealityUpgrade.scourToEmpower.isBought) return new Decimal(1);
-      const sac = player.reality.glyphs.sac.dilation.add(added ?? 0);
+      const sac = player.disablePostReality ? DC.D0 : player.reality.glyphs.sac.dilation.add(added ?? 0);
       const capped = Decimal.clampMax(sac, GlyphSacrificeHandler.maxSacrificeForEffects);
       const exponent = Decimal.pow(new Decimal(Decimal.log10(capped.add(1))).div(100), 0.1).times(0.32);
       return Decimal.pow(Decimal.clampMin(capped, 1), exponent);
@@ -75,7 +75,7 @@ export const glyphSacrifice = {
     id: "effarig",
     effect: added => {
       if (Pelle.isDisabled("glyphsac") && !PelleRealityUpgrade.scourToEmpower.isBought) return new Decimal(0);
-      const sac = player.reality.glyphs.sac.effarig.add(added ?? 0);
+      const sac = player.disablePostReality ? DC.D0 : player.reality.glyphs.sac.effarig.add(added ?? 0);
       // This doesn't use the GlyphSacrificeHandler cap because it hits its cap (+100%) earlier
       const capped = Decimal.clampMax(sac, 1e70);
       return new Decimal(Decimal.log10(capped.div(1e20).add(1))).times(2);
@@ -87,7 +87,7 @@ export const glyphSacrifice = {
     id: "reality",
     effect: added => {
       if (Pelle.isDisabled("glyphsac") && !PelleRealityUpgrade.scourToEmpower.isBought) return new Decimal(0);
-      const sac = player.reality.glyphs.sac.reality.add(added ?? 0);
+      const sac = player.disablePostReality ? DC.D0 : player.reality.glyphs.sac.reality.add(added ?? 0);
       // This cap is only feasibly reached with the imaginary upgrade, but we still want to cap it at a nice number
       return Decimal.clampMax(Decimal.sqrt(sac).div(15).add(1), 100);
     },
