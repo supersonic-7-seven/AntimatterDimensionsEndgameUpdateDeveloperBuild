6a3c97a9e9ff62f334a2ab69d68eeeb3bc11615d
Supersonic Seven <b.rockholz@icloud.com>
2026-01-30 15:10:18 -0500

    Update ethereal.js
    Finalizing stars?

--- src/core/ethereal.js
+++ src/core/ethereal.js
@@ -9,10 +9,52 @@ export const Ethereal = {
     return Decimal.pow(this.cosmicSector, this.cosmicSector);
   },
   get sectorBoost() {
-    //if (Alpha.isRunning) return Decimal.pow(2, this.cosmicSector - 1);
+    if (Alpha.isRunning) return Decimal.pow(2, this.cosmicSector - 1);
     return Decimal.pow(2, Math.pow(this.cosmicSector - 1, 2));
+  },
+  get isExtended() {
+    return player.endgame.ethereal.isExtended;
+  },
+  get nextStar() {
+    return EtherealStars.all.find(x => !x.isUnlocked);
+  },
+  get nextStarUnlockStage() {
+    return this.nextStar?.unlockStage;
   }
 };
+
+export class EtherealStarState {
+  constructor(config) {
+    this.config = config;
+  }
+
+  get id() {
+    return this.config.id;
+  }
+
+  get reward() {
+    return this.config.effect();
+  }
+
+  get unlockStage() {
+    return this.config.unlockStage;
+  }
+
+  get isUnlocked() {
+    return Alpha.currentStage >= this.unlockStage;
+  }
+
+  get canGainStar() {
+    return Currency.etherealPower.gte(this.config.resetReq);
+  }
+}
+
+export const EtherealStars = mapGameDataToObject(
+  GameDatabase.endgame.stars,
+  config => (config.isBaseResource
+    ? new EtherealStarState(config)
+    : new EtherealStarState(config))
+);
 
 export function getEtherealPowerGainPerSecond() {
   const cpFactor = Decimal.pow(Decimal.log10(player.endgame.celestialPoints.add(1)).div(100), 10);
@@ -25,4 +67,14 @@ export function getEtherealPowerGainPerSecond() {
 export function tryAdvanceSector() {
   if (Currency.etherealPower.lt(Ethereal.sectorThreshold)) return;
   player.endgame.ethereal.sector++;
+}
+
+export function resetForStar(id) {
+  const gainedStarType = EtherealStars.all.find(x => x.id === id);
+  const starName = gainedStarType.name;
+  const resetReq = gainedStarType.resetReq;
+  if (Currency.etherealPower.lt(resetReq)) return;
+  const resetFormula = Decimal.pow(Currency.etherealPower.div(resetReq), 0.5 - id / 20);
+  player.endgame.ethereal.power = DC.D0;
+  player.endgame.ethereal.star[starName] = player.endgame.ethereal.star[starName].add(resetFormula);
 }
