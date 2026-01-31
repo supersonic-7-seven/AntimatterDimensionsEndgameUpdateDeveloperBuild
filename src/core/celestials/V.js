13ef3c60b471eda1048d829284b28c83514abef0
Supersonic Seven <b.rockholz@icloud.com>
2026-01-30 19:26:13 -0500

    Update V.js
    Establish

--- src/core/celestials/V.js
+++ src/core/celestials/V.js
@@ -225,7 +225,7 @@ export const V = {
       if (i < 6) sum += player.celestials.v.runUnlocks[i];
       else sum += player.celestials.v.runUnlocks[i] * 2;
     }
-    this.spaceTheorems = sum * (ExpansionPack.vPack.isBought ? 2 : 1) * Ra.unlocks.spaceTheoremBoost.effectOrDefault(1);
+    this.spaceTheorems = player.disablePostReality ? 0 : sum * (ExpansionPack.vPack.isBought ? 2 : 1) * Ra.unlocks.spaceTheoremBoost.effectOrDefault(1);
   },
   reset() {
     player.celestials.v = {
