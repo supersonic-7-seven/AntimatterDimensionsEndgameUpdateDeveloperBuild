f1e150d17cac9d485b2e954b468fc78523b29ed3
Supersonic Seven <b.rockholz@icloud.com>
2026-01-30 22:06:40 -0500

    Update pelle.js
    Unix

--- src/core/celestials/pelle/pelle.js
+++ src/core/celestials/pelle/pelle.js
@@ -521,7 +521,7 @@ export const Pelle = {
   // Calculations assume this is in units of proportion per second (eg. 0.03 is 3% drain per second)
   get riftDrainPercent() {
     const extraDrain = Math.min(0.45, player.endgames * 0.05);
-    if (EndgameMilestone.riftFill.isReached) return 0.05 + extraDrain;
+    if (EndgameMilestone.riftFill.isReached && !player.disablePostReality) return 0.05 + extraDrain;
     return 0.05;
   },
 
