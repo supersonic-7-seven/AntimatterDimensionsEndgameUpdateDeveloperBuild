1b8e74440018a8bc462db10f5f0aed3bd094d79e
Supersonic Seven <b.rockholz@icloud.com>
2026-01-30 19:54:59 -0500

    Update glyph-generator.js
    Emancify

--- src/core/glyphs/glyph-generator.js
+++ src/core/glyphs/glyph-generator.js
@@ -237,7 +237,7 @@ export const GlyphGenerator = {
   randomStrength(rng) {
     // Technically getting this upgrade really changes glyph gen but at this point almost all
     // the RNG is gone anyway.
-    if (Ra.unlocks.maxGlyphRarityAndShardSacrificeBoost.canBeApplied) return rarityToStrength(100 + Ra.unlocks.rarityBuff.effectOrDefault(0));
+    if (Ra.unlocks.maxGlyphRarityAndShardSacrificeBoost.canBeApplied && !player.disablePostReality) return rarityToStrength(100 + Ra.unlocks.rarityBuff.effectOrDefault(0));
     let result = GlyphGenerator.gaussianBellCurve(rng) * GlyphGenerator.strengthMultiplier;
     const relicShardFactor = (Ra.unlocks.extraGlyphChoicesAndRelicShardRarityAlwaysMax.canBeApplied || EndgameMastery(53).isBought) && !player.disablePostReality ? 1 : rng.uniform();
     const increasedRarity = relicShardFactor * Effarig.maxRarityBoost +
