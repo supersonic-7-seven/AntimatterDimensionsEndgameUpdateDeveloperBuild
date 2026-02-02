f5ef52a54c2f79d3b5e4d22ce1a6674f568c0501
Supersonic Seven <b.rockholz@icloud.com>
2026-02-02 12:34:53 -0500

    Update constants.js
    Call variable

--- src/core/constants.js
+++ src/core/constants.js
@@ -83,6 +83,7 @@ window.DC = deepFreeze({
   C1D1_1245:                Decimal.div(1, 1.1245),
   D2P30D0_61:               Decimal.pow(2, 30 / 0.61),
   C2P30:                    Decimal.pow(2, 30),
+  C2P140:                   Decimal.pow(2, 140),
   C2P1024:                  Decimal.pow(2, 1024),
   C10P16000D3:              Decimal.pow(10, 16000 / 3),
 
