057d074afd3490bedd0c27b65689b92547bc2527
Supersonic Seven <b.rockholz@icloud.com>
2026-02-04 13:27:24 -0500

    Update endgame.js
    Revert

--- src/core/endgame.js
+++ src/core/endgame.js
@@ -28,7 +28,7 @@ function giveEndgameRewards() {
   let endgameMultiplier = ((ExpansionPack.enslavedPack.isBought && !player.disablePostReality)
     ? Math.floor(1 + Math.pow(Math.log10(Math.min(Tesseracts.effectiveCount, 1000) * Math.max(Math.log10(Tesseracts.effectiveCount) - 2, 1) + 1), Math.log10(player.endgames + 1)))
     : 1);
-  endgameMultiplier *= Math.pow(2, Alpha.currentStage);
+  endgameMultiplier *= Math.pow(1.25, Alpha.currentStage);
   Currency.celestialPoints.add(gainedCelestialPoints());
   Currency.doomedParticles.add(gainedDoomedParticles());
   updateEndgameRecords();
