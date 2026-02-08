fdb3f73386705c8d2e68a8bf0c5b5b3b1b7a5e5a
Supersonic Seven <b.rockholz@icloud.com>
2026-02-08 16:03:42 -0500

    Update alpha.js
    Blaze

--- src/core/secret-formula/celestials/alpha.js
+++ src/core/secret-formula/celestials/alpha.js
@@ -51,12 +51,12 @@ export const alphaUnlocks = {
   breakInfinity: {
     id: 5,
     requirement: 6,
-    nerfDescription: () => `Break Infinty Upgrades are ${formatX(1000)} more expensive, post-Break Tickspeed/Dimension cost scalings start at ${formatX(20)}, IPow Conversion Rate /${format(DC.E1.div(Decimal.log10(Decimal.log10(Currency.infinityPoints.value.add(1)).add(1)).pow(2)).toNumber(), 2, 2)} (based on IP)`,
+    nerfDescription: () => `Break Infinty Upgrades are ${formatX(1000)} more expensive, post-Break Tickspeed/Dimension cost scalings start at ${formatX(20)}, IPow Conversion Rate /${format(DC.D7.div(Decimal.log10(Decimal.log10(Currency.infinityPoints.value.add(1)).add(1)).pow(2)).toNumber(), 2, 2)} (based on IP)`,
     buffDescription: () => `Reduce Post-Break Tickspeed cost scaling by ${format(0.15, 2, 2)} and Post-Break Dimension cost scaling by ${format(0.25, 2, 2)}`,
     effects: {
       nerfA: 1000,
       nerfB: 20,
-      nerfC: () => DC.E1.div(Decimal.log10(Decimal.log10(Currency.infinityPoints.value.add(1)).add(1)).pow(2)).toNumber(),
+      nerfC: () => DC.D7.div(Decimal.log10(Decimal.log10(Currency.infinityPoints.value.add(1)).add(1)).pow(2)).toNumber(),
       buffA: 0.15,
       buffB: 0.25
     }
