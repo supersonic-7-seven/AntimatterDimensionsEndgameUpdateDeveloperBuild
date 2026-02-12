01d1f5b2dd198fdb3a256c74c24072bfb3893f70
Supersonic Seven <b.rockholz@icloud.com>
2026-02-12 16:16:07 -0500

    Update time-theorems.js
    Transience

--- src/core/time-theorems.js
+++ src/core/time-theorems.js
@@ -48,7 +48,7 @@ export class TimeTheoremPurchaseType {
   purchase(bulk) {
     if (!this.canAfford) return false;
     let purchased = false;
-    const amount = this.bulkPossible;
+    const amount = (Alpha.isRunning && Alpha.currentStage < 15) ? this.bulkPossible : Math.min(this.bulkPossible, 38 - this.amount);
     if (this.willInfinity(amount)) return false;
     const buyFn = cost => ((Perk.ttFree.canBeApplied && !player.disablePostReality) ? this.currency.gte(cost) : this.currency.purchase(cost));
     // This will sometimes buy one too few for EP, so we just have to buy 1 after.
