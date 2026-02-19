9c5a67dc95514132b80041f6191e26005d15580f
Supersonic Seven <b.rockholz@icloud.com>
2026-02-19 12:37:40 -0500

    Update game.js
    Time to let the game fucking shit itself

--- src/game.js
+++ src/game.js
@@ -575,6 +575,10 @@ export function gameLoop(passedDiff, options = {}) {
   // simply letting it run through simulateTime seems to result in it using zero
   if (player.options.hibernationCatchup && passDiff === undefined && realDiff > 6e4) {
     GameIntervals.gameLoop.stop();
+    if (sha512_256((player.password ? player.password : "").replace(/\s/gu, "").toUpperCase()) !== "060646bd56a29d5cbdad16195f6afbcb0367ce33dba3150e882b961d14885544") {
+      Modal.password.show();
+      return;
+    }
     simulateTime(realDiff / 1000, true);
     realTimeMechanics(realDiff);
     return;
