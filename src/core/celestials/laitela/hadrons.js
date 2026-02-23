096272f70e19acc29f8b01731515f6a895d50eb9
Supersonic Seven <b.rockholz@icloud.com>
2026-02-23 15:26:51 -0500

    Update hadrons.js
    Pressdown

--- src/core/celestials/laitela/hadrons.js
+++ src/core/celestials/laitela/hadrons.js
@@ -6,18 +6,18 @@ export const Hadrons = {
     return Time.thisEndgameRealTime._ms.div(36000);
   },
   get singularityMultiplier() {
-    return Decimal.pow10(new Decimal(this.hadrons.light).times(this.timeFactor.times(4).min(100)).times(10));
+    return DualityUpgrade(15).isBought ? Decimal.pow10(new Decimal(this.hadrons.light).times(this.timeFactor.times(4).min(100)).times(10)) : DC.D1;
   },
   get darkMatterCapMultiplier() {
-    return Decimal.pow10(new Decimal(this.hadrons.light).times(this.timeFactor.times(2).min(100)).times(100));
+    return DualityUpgrade(16).isBought ? Decimal.pow10(new Decimal(this.hadrons.light).times(this.timeFactor.times(2).min(100)).times(100)) : DC.D1;
   },
   get darkEnergyAscensionBoost() {
-    return new Decimal(this.hadrons.light).times(this.timeFactor.min(100)).toNumber();
+    return DualityUpgrade(17).isBought ? new Decimal(this.hadrons.light).times(this.timeFactor.min(100)).toNumber() : 0;
   },
   get entropyFormulaBoost() {
-    return Decimal.pow(Decimal.log10(this.timeFactor.div(2).max(1).min(100)).times(2).add(1), this.hadrons.light);
+    return DualityUpgrade(18).isBought ? Decimal.pow(Decimal.log10(this.timeFactor.div(2).max(1).min(100)).times(2).add(1), this.hadrons.light) : DC.D1;
   },
   get continuumMultiplier() {
-    return Decimal.pow(Decimal.log10(this.timeFactor.div(5).max(1).min(100)).div(2).add(1), this.hadrons.dark);
+    return DualityUpgrade(19).isBought ? Decimal.pow(Decimal.log10(this.timeFactor.div(5).max(1).min(100)).div(2).add(1), this.hadrons.dark) : 1;
   }
 };
