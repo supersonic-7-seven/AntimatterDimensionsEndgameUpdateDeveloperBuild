478ad6fa753b062c955e503c49a0b91ce1795f93
Supersonic Seven <b.rockholz@icloud.com>
2026-02-23 13:22:26 -0500

    Update laitela.js
    Raise

--- src/core/celestials/laitela/laitela.js
+++ src/core/celestials/laitela/laitela.js
@@ -48,8 +48,9 @@ export const Laitela = {
   },
   get matterExtraPurchaseFactor() {
     if ((Pelle.isDoomed && !PelleDestructionUpgrade.continuumBuff.isBought) || player.disablePostReality) return 1;
-    return (Decimal.pow(new Decimal(Decimal.log10(Currency.darkMatter.max.add(1))).div(50), 0.4).times(0.5).add(1).times(
-      SingularityMilestone.continuumMult.effectOrDefault(new Decimal(0)).add(1)).times(DualityUpgrade(11).effectOrDefault(1))).toNumber();
+    return (Decimal.pow(Decimal.pow(new Decimal(Decimal.log10(Currency.darkMatter.max.add(1))).div(50), 0.4).times(0.5).add(1).times(
+      SingularityMilestone.continuumMult.effectOrDefault(new Decimal(0)).add(1)).times(
+      DualityUpgrade(11).effectOrDefault(1))), DualityUpgrade(14).effectOrDefault(1)).toNumber();
   },
   get hadronizes() {
     return this.celestial.hadronizes;
