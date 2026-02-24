0aa7e84ffa87c79c44548a99b56e0fa24cd6fd6e
Supersonic Seven <b.rockholz@icloud.com>
2026-02-24 07:45:44 -0500

    Update machines.js
    Harsher

--- src/core/machines.js
+++ src/core/machines.js
@@ -122,8 +122,8 @@ export const MachineHandler = {
     return Decimal.pow(Decimal.clampMin(this.uncappedIM.add(1).log10().sub(1000), 0), Decimal.clampMin(
       Decimal.log10(Currency.realityMachines.value.add(1).log10().add(1)).sub(3), 1).times(
       Decimal.clampMin(Decimal.log10(Decimal.log10(Decimal.log10(this.uncappedRM.add(1)).add(1)).add(1)).sub(
-      1.75).times(12).max(0.75).add(1), 1).add(Decimal.clampMin(Decimal.log10(Decimal.log10(Decimal.log10(
-      this.uncappedRM.add(1)).add(1)).add(1)).sub(1.8125).times(5), 0))));
+      1.75).times(12).max(0.6).add(1), 1).add(Decimal.clampMin(Decimal.log10(Decimal.log10(Decimal.log10(
+      this.uncappedRM.add(1)).add(1)).add(1)).sub(1.8).times(4), 0))));
   },
 
   get currentDMCap() {
