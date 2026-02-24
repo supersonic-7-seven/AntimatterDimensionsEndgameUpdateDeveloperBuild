a4d46447b8d56ad70c80b402cdc24b32ba1b0619
Supersonic Seven <b.rockholz@icloud.com>
2026-02-24 18:53:02 -0500

    Update auto-glyph-processor.js
    I’m sorry...

--- src/core/glyphs/auto-glyph-processor.js
+++ src/core/glyphs/auto-glyph-processor.js
@@ -340,6 +340,7 @@ export function getGlyphLevelInputs() {
   scaledLevel = instabilitySoftcap(scaledLevel, staticFactors.hyperInstability, 400);
   scaledLevel = instabilitySoftcap(scaledLevel, staticFactors.extremeInstability, (EndgameUpgrade(13).isBought && !player.disablePostReality) ? 5 : 1);
   scaledLevel = instabilitySoftcap(scaledLevel, staticFactors.immenseInstability, 1);
+  scaledLevel = instabilitySoftcap(scaledLevel, staticFactors.extensiveInstability, 0.1);
 
   const scalePenalty = scaledLevel > 0 ? baseLevel / scaledLevel : 1;
   const incAfterInstability = staticFactors.realityUpgrades + staticFactors.achievements;
@@ -373,6 +374,7 @@ export function staticGlyphWeights() {
   const hyperInstability = Glyphs.hyperInstabilityThreshold;
   const extremeInstability = Glyphs.extremeInstabilityThreshold;
   const immenseInstability = Glyphs.immenseInstabilityThreshold;
+  const extensiveInstability = Glyphs.extensiveInstabilityThreshold;
   const realityUpgrades = [Array.range(1, 5).every(x => RealityUpgrade(x).boughtAmount > 0)]
     .concat(Array.range(1, 4).map(x => Array.range(1, 5).every(y => RealityUpgrade(5 * x + y).isBought)))
     .filter(x => x)
@@ -384,6 +386,7 @@ export function staticGlyphWeights() {
     hyperInstability,
     extremeInstability,
     immenseInstability,
+    extensiveInstability,
     realityUpgrades,
     achievements
   };
