411b46497f6e41687e714a0addf9bc603742b702
Supersonic Seven <b.rockholz@icloud.com>
2026-02-24 20:14:18 -0500

    Update alpha.js
    How many must it take

--- src/core/secret-formula/celestials/alpha.js
+++ src/core/secret-formula/celestials/alpha.js
@@ -262,10 +262,10 @@ export const alphaUnlocks = {
   timeDimension8: {
     id: 26,
     requirement: 27,
-    nerfDescription: () => `Eternity Point gain is raised ${formatPow(Math.clamp(1 - Decimal.log10(player.records.thisEternity.maxIP.add(1)).sub(1.5e6).div(1e6).toNumber(), 0, 1), 2, 3)}`,
+    nerfDescription: () => `Eternity Point gain is raised ${formatPow(Math.clamp(1 - Decimal.log10(player.records.thisEternity.maxIP.add(1)).sub(1.5e6).div(2.5e6).toNumber(), 0, 1), 2, 3)}`,
     buffDescription: () => `The ${formatInt(8)}th Time Dimension is powered ${formatInt(1000)}`,
     effects: {
-      nerf: () => Math.clamp(1 - Decimal.log10(player.records.thisEternity.maxIP.add(1)).sub(1.5e6).div(1e6).toNumber(), 0, 1),
+      nerf: () => Math.clamp(1 - Decimal.log10(player.records.thisEternity.maxIP.add(1)).sub(1.5e6).div(2.5e6).toNumber(), 0, 1),
       buff: 1000
     }
   },
