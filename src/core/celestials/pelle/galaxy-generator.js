cee60beb173c990e35eb8a03de27e891bd1c21ae
Supersonic Seven <b.rockholz@icloud.com>
2026-02-28 12:56:54 -0500

    Update galaxy-generator.js
    ka BOOM

--- src/core/celestials/pelle/galaxy-generator.js
+++ src/core/celestials/pelle/galaxy-generator.js
@@ -131,6 +131,7 @@ export const GalaxyGenerator = {
     if (this.sacrificeActive) {
       let reductionSpeed = 0.075;
       if (EndgameMilestone.galGenAnimation.isReached && !player.disablePostReality) reductionSpeed = reductionSpeed * Math.pow(1.2, Math.floor(Math.min(Currency.endgames.value, 100) / 5));
+      if (Alpha.isDestroyed) reductionSpeed = 1e300;
       this.capRift.reducedTo = Decimal.max(new Decimal(this.capRift.reducedTo).sub(new Decimal(reductionSpeed).times(diff).div(1000)), 0).toNumber();
       if (this.capRift.reducedTo === 0) {
         player.celestials.pelle.galaxyGenerator.sacrificeActive = false;
