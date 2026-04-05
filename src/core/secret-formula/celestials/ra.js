c2c79fe2ba3d7a97727d5d8ccfa67ab21d928337
Supersonic Seven <b.rockholz@icloud.com>
2026-04-05 17:02:00 -0400

    Rebalancing Teresa Level 125 for hopefully the final time

--- src/core/secret-formula/celestials/ra.js
+++ src/core/secret-formula/celestials/ra.js
@@ -354,7 +354,7 @@ export const ra = {
     celestialDimensionPower: {
       id: 34,
       reward: "Endgames and Teresa level empower Celestial Dimensions",
-      effect: () => player.disablePostReality ? 1 : Math.pow(1 + Math.log10(1 + (Math.pow(Math.floor(Ra.pets.teresa.level) * Math.log10(player.endgames + 1), 3))), 0.5),
+      effect: () => player.disablePostReality ? 1 : Math.pow(Math.max(Ra.pets.teresa.level * Math.log10(player.endgames + 1) / 2000, 1), 5),
       pet: "teresa",
       level: 125,
       displayIcon: `<span class="fas fa-award"></span>`,
