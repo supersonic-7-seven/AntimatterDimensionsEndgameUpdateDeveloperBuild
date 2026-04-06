3bf9c25733eb8f7b546c4da6753c93e4c5b9844b
Supersonic Seven <b.rockholz@icloud.com>
2026-04-06 13:54:02 -0400

    Create celestial-tickspeed-autobuyer.js

---
+++ src/core/autobuyers/celestial-tickspeed-autobuyer.js
@@ -0,0 +1,64 @@
+import { IntervaledAutobuyerState } from "./autobuyer";
+
+export class CelestialTickspeedAutobuyerState extends IntervaledAutobuyerState {
+  get data() {
+    return player.auto.celestialTickspeed;
+  }
+
+  get name() {
+    return `Celestial Tickspeed`;
+  }
+
+  get isUnlocked() {
+    return CelestialBreakInfinityUpgrade.autoCDPlus;
+  }
+
+  get interval() {
+    return 1000;
+  }
+
+  get mode() {
+    return this.data.mode;
+  }
+
+  set mode(value) {
+    this.data.mode = value;
+  }
+
+  get hasUnlimitedBulk() {
+    return this.mode === AUTOBUYER_MODE.BUY_MAX;
+  }
+
+  toggleMode() {
+    this.mode = [
+      AUTOBUYER_MODE.BUY_SINGLE,
+      AUTOBUYER_MODE.BUY_MAX
+    ]
+      .nextSibling(this.mode);
+  }
+
+  get canTick() {
+    return CelestialTickspeed.isAvailableForPurchase && CelestialTickspeed.isAffordable && super.canTick;
+  }
+
+  tick() {
+    super.tick();
+    switch (this.mode) {
+      case AUTOBUYER_MODE.BUY_SINGLE:
+        buyCelestialTickSpeed();
+        break;
+      case AUTOBUYER_MODE.BUY_MAX:
+        buyCelestialMaxTickSpeed();
+        break;
+    }
+  }
+
+  get resetTickOn() {
+    return PRESTIGE_EVENT.ENDGAME;
+  }
+
+  reset() {
+    super.reset();
+    this.data.mode = AUTOBUYER_MODE.BUY_SINGLE;
+  }
+}
