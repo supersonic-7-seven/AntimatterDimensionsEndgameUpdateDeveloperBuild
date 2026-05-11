cd4029a29d85e9c132b57b45f2f0be1d7d21c90c
Supersonic Seven <b.rockholz@icloud.com>
2026-05-11 12:23:49 -0400

    Update gainedDivineStars to use thisCondense.maxVM

--- src/game.js
+++ src/game.js
@@ -464,7 +464,7 @@ export function gainedCelestialEternityPoints() {
 
 export function gainedDivineStars() {
   const div = 308;
-  let divs = Decimal.pow10(player.records.thisStarRun.maxVM.add(1).log10().div(div).sub(0.75));
+  let divs = Decimal.pow10(player.records.thisCondense.maxVM.add(1).log10().div(div).sub(0.75));
 
   return divs.floor();
 }
