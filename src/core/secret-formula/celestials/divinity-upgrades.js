0c2f70083ab59e437211940b159148aa93e81321
Supersonic Seven <b.rockholz@icloud.com>
2026-05-11 18:57:31 -0400

    Add new divine upgrades for layer 2

--- src/core/secret-formula/celestials/divinity-upgrades.js
+++ src/core/secret-formula/celestials/divinity-upgrades.js
@@ -80,4 +80,85 @@ export const divinityUpgrades = {
     description: () => `Reduce the time for Dual Machines to approach their cap by ${formatPercents(0.5)}`,
     effect: 0.5
   },
+  divineL2U1: {
+    name: "Stellar Compound",
+    id: "divineL2U1",
+    layer: 2,
+    cost: new Decimal(1),
+    description: "Divine Dimensions gain a multiplier based on real time since your last Condense",
+    effect: () => Decimal.pow(Time.thisCondenseRealTime.totalSeconds.add(1), 3),
+    formatEffect: value => formatX(value, 2, 2)
+  },
+  divineL2U2: {
+    name: "Solar Flares",
+    id: "divineL2U2",
+    layer: 2,
+    cost: new Decimal(7),
+    description: "Divine Stars mutliply Divine Energy production",
+    effect: () => player.celestials.pelle.divinity.divineStars.max(1),
+    formatEffect: value => formatX(value, 2)
+  },
+  divineL2U3: {
+    name: "Postmortal",
+    id: "divineL2U3",
+    layer: 2,
+    cost: new Decimal(17),
+    description: "Divine Stars boost Ethereal Power production",
+    effect: () => Decimal.pow(Decimal.log10(player.celestials.pelle.divinity.divineStars.add(1)).add(1), 7),
+    formatEffect: value => formatX(value, 2, 2)
+  },
+  divineL2U4: {
+    name: "Electrify",
+    id: "divineL2U4",
+    layer: 2,
+    cost: new Decimal(77),
+    description: "Divine Energy base gain is now based on your highest-ever Divine Matter"
+  },
+  divineL2U5: {
+    name: "Cannot Afford Loss",
+    id: "divineL2U5",
+    layer: 2,
+    cost: new Decimal(277),
+    description: "Keep all Layer One Divine Upgrades on Condense"
+  },
+  divineL2U6: {
+    name: "Headstart",
+    id: "divineL2U6",
+    layer: 2,
+    cost: new Decimal(777),
+    description: () => `Start Condense with ${format(5e36, 2, 2)} Divine Matter`,
+    effect: 5e36
+  },
+  divineL2U7: {
+    name: "Gravitate",
+    id: "divineL2U7",
+    layer: 2,
+    cost: new Decimal(2777),
+    description: () => `Divine Dimensions are raised ${formatPow(1.1, 2, 3)}`,
+    effect: 1.1
+  },
+  divineL2U8: {
+    name: "Liftoff",
+    id: "divineL2U8",
+    layer: 2,
+    cost: new Decimal(7777),
+    description: () => `The Divine Dimension Per-Purchase Multiplier is increased to ${formatX(17)}`
+  },
+  divineL2U9: {
+    name: "Ascension",
+    id: "divineL2U9",
+    layer: 2,
+    cost: new Decimal(77777),
+    description: "Divine Dimensions are multiplied based on Celestial Matter",
+    effect: Decimal.pow(Decimal.log10(Currency.celestialMatter.value).add(1), 7),
+    formatEffect: value => formatX(value, 2, 2)
+  },
+  divineL2U10: {
+    name: "Completion",
+    id: "divineL2U10",
+    layer: 2,
+    cost: new Decimal(1777777),
+    description: () => `Divine Matter effects are set to highest-ever Divine Matter and Divine Energy gain
+      is always produced at a ${formatPercents(1)} rate with no penalty`
+  },
 };
