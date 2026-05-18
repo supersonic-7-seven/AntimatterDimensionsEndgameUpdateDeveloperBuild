66e90244c32c3e49305c1b0d813d0ad0dfa14aca
Supersonic Seven <b.rockholz@icloud.com>
2026-05-18 13:21:25 -0400

    Update galaxyReset to include LHC condition
    Add condition to galaxyReset function for LHC and limerick2.

--- src/core/galaxy.js
+++ src/core/galaxy.js
@@ -181,7 +181,8 @@ function galaxyReset() {
   EventHub.dispatch(GAME_EVENT.GALAXY_RESET_BEFORE);
   player.galaxies = player.galaxies.add(1);
   if ((!Achievement(143).isUnlocked || ((Pelle.isDoomed && !PelleAchievementUpgrade.achievement143.canBeApplied) &&
-    !PelleUpgrade.galaxyNoResetDimboost.canBeApplied)) || (player.disablePostReality && !(Alpha.isRunning && Alpha.currentStage >= 20))) {
+    !PelleUpgrade.galaxyNoResetDimboost.canBeApplied)) || (player.disablePostReality && !(Alpha.isRunning && Alpha.currentStage >= 20) &&
+    !(LHC.voidRunning && NullUpgrade.limerick2.isBought))) {
     player.dimensionBoosts = new Decimal(0);
   }
   softReset(0);
