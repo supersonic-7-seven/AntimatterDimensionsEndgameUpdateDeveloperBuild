3a557ba31df80cef7453015c0355538f944b6eb2
Supersonic Seven <b.rockholz@icloud.com>
2026-06-23 11:43:48 -0400

    Add partCondensed property to player state

--- src/core/player.js
+++ src/core/player.js
@@ -1052,6 +1052,7 @@ window.player = {
         isProducingEnergy: false,
         condenses: DC.D0,
         divineStars: DC.D0,
+        partCondensed: DC.D0,
         supernovae: DC.D0,
         nebulae: DC.D0
       },
